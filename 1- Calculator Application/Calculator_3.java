6ee5799c019976f3a2447f4795216dc818d2b10b
AliMahmoud26 <alimah.engineer@gmail.com>
2026-04-04 18:09:31 +0200

    Revising the code of calculator App

--- 1- Calculator Application/Calculator_3.java
+++ 1- Calculator Application/Calculator_3.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class Calculator_3 {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the user
         Scanner input = new Scanner(System.in);
-        // Declaring the variables of the program
+
         double num1, num2, result;
         char operator;
-        // Prompt the user for the first number
-        System.out.print("Enter your first number: ");
-        num1  = input.nextDouble();
-        // Prompt the user for the operator
-        System.out.print("Enter an operator (+, -, *, /): ");
+
+        System.out.println("Enter your first number: ");
+        num1 = input.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /): ");
         operator = input.next().charAt(0);
-        // Prompt the user for the second number
+
+        System.out.println("Enter your second number: ");
         num2 = input.nextDouble();
 
-        // Using switch case to perform the calculation
-        switch (operator) {
+        switch(operator) {
             case '+' -> {
                 result = num1 + num2;
                 System.out.println("Result: " + result);
@@ -27,19 +26,19 @@ public class Calculator_3 {
                 System.out.println("Result: " + result);
             }
             case '*' -> {
-                result =  num1 * num2;
+                result = num1 * num2;
                 System.out.println("Result: " + result);
             }
             case '/' -> {
                 if (num2 == 0) {
-                    System.out.print("Error: Division by zero is not allowed.");
+                    System.out.println("Error: Division by zero is not allowed.");
                 } else {
                     result = num1 / num2;
                     System.out.println("Result: " + result);
                 }
             }
             default -> {
-                System.out.print("Error: Invalid operator.");
+                System.out.println("Error: Invalid operator.");
             }
         }
     }
