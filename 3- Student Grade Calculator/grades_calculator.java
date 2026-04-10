94a496bbaee0180d0d3ebd1aa161b90f9c5df79a
AliMahmoud26 <alimah.engineer@gmail.com>
2026-04-10 19:15:11 +0200

    Finishing up calculating studend grade calculator

--- 3- Student Grade Calculator/grades_calculator.java
+++ 3- Student Grade Calculator/grades_calculator.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class grades_calculator {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Student Grade Calculator!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+
+        List<String> studentNames = new ArrayList<>();
+        List<Double> studentGrades = new ArrayList<>();
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.print("Enter the name of student " + (i + 1) + ": ");
+            String name = scanner.next();
+            studentNames.add(name);
+
+            System.out.print("Enter the grade of student " + (i + 1) + ": ");
+            double grade = scanner.nextDouble();
+            studentGrades.add(grade);
+        }
+
+        double average = calculateAverage(studentGrades);
+
+        System.out.println("\nStudent Grade Report:");
+        
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("Student: " + studentNames.get(i) + ", Grade: " + studentGrades.get(i));
+        }
+
+        System.out.println("\nAverage Grade: " + average);
+    }
+
+    private static double calculateAverage(List<Double> grades) {
+        double sum = 0.0;
+        for (Double grade: grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
     }
 }
